ebafbb0cf8c00e054bcc7583473343ae82df98f1
VarishTyagi <varishtyagi351@gmail.com>
2024-08-12 22:07:38 +0530

    first commit

---
+++ frentend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TaskManager from './TaskManager';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
