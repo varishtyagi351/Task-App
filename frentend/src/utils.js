ab5dc65ccca61d09e79da24e6317246038461be7
Varish Tyagi <114216483+varishtyagi351@users.noreply.github.com>
2024-08-12 22:19:44 +0530

    Update utils.js

--- frentend/src/utils.js
+++ frentend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://task-app-api-cyan.vercel.app';
